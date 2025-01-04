f90912ab4e757e80f0f289634004cff2ad28043f
Sharath <sharath03.tech@gmail.com>
2025-01-04 15:39:03 +0530

    established database connection successfully

--- app.js
+++ app.js
@@ -1,30 +1,61 @@
-import express, { json } from 'express';
-import porductRoutes from './src/features/product/product.routes.js';
-import userRoutes from './src/features/user/user.routes.js';
-import cartRoutes from './src/features/cart/cartItem.routes.js'
+import express, { json } from "express";
+import porductRoutes from "./src/features/product/product.routes.js";
+import userRoutes from "./src/features/user/user.routes.js";
+import cartRoutes from "./src/features/cart/cartItem.routes.js";
 //import basicAuth from './src/middlewares/user/basicAuth.middleware.js';
-import jwtAuth from './src/middlewares/user/jwt.middleware.js';
-import swaggerUi from 'swagger-ui-express';
+import jwtAuth from "./src/middlewares/user/jwt.middleware.js";
+import swaggerUi from "swagger-ui-express";
 //import swaggerDocument from './swagger.json' assert {type : "json"};
-import swaggerDocument from './swagger3.json' assert {type : "json"};
+import swaggerDocument from "./swagger3.json" assert { type: "json" };
 import cors from "cors";
-import logger from './src/middlewares/user/logger.middleware.js';
-import ApplicationError from './src/error_handler/app.error.js';
+import logger from "./src/middlewares/user/logger.middleware.js";
+import ApplicationError from "./src/error_handler/app.error.js";
 
-const app = express()
-const port = 3000
+//database
+import { MongoClient } from "mongodb";
+import { cli } from "winston/lib/winston/config/index.js";
+
+const app = express();
+const port = 3000;
+
+//********************database starts Here***
+
+//creating connection to database
+const client = new MongoClient("mongodb://localhost:27017/");
+
+//database name
+const dbName = "Movies";
+
+//function for client to connect to db
+async function main() {
+  //connect method to connect to db
+  await client.connect();
+  //console.log("connected successfully database");
+  const db = client.db(dbName);
+  const collection = db.collection("highest_rated");
+  const movies = await collection.find({}).toArray();
+  //console.log("found doucment => : ", movies);
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close);
+
+//**********************database ends Here***
 
 //CORS library/package
 const corsOptions = {
-  origin : 'http://127.0.0.1:5500',
-  optionSuccessStatus : 200
-}
+  origin: "http://127.0.0.1:5500",
+  optionSuccessStatus: 200,
+};
 app.use(cors());
 
 app.use(express.json());
 
 //parsing the data
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
 //handle CORS
 // app.use((req, res, next) => {
@@ -37,39 +68,39 @@ app.use(express.urlencoded({extended : true}));
 //   next();
 // })
 
-
 //swagger docs
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //app.use('/api/product',basicAuth, porductRoutes);
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 
 app.use(logger);
 
-app.use('/api/product',jwtAuth, porductRoutes);
+app.use("/api/product", jwtAuth, porductRoutes);
 
-app.use('/api/cart', jwtAuth, cartRoutes);
+app.use("/api/cart", jwtAuth, cartRoutes);
 
-
-app.get('/', (req, res)=>{
+app.get("/", (req, res) => {
   res.send("hello from rest api");
 });
 
 //send error message if user provided route does'nt match  with the available routes
-app.use((req, res)=> {
-  res.status(404).send("Page Not Found, check our API docs here : localhost:3000/api-docs");
+app.use((req, res) => {
+  res
+    .status(404)
+    .send("Page Not Found, check our API docs here : localhost:3000/api-docs");
 });
 
 //error handling middleware
 app.use((err, req, res, next) => {
   if (err instanceof ApplicationError) {
-    const {code, message} = err;
+    const { code, message } = err;
     res.status(code).send(message);
   }
   console.error(err.stack);
   res.status(500).send("something went wrong!");
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -1,105 +1,106 @@
 import ProductModel from "./product.model.js";
 
-export default class ProductController{
-    //get all products{}
-    getAllProducts(req,res){
-        const products = ProductModel.getAllProducts();
-        return res.status(200).json(products);
+export default class ProductController {
+  //get all products{}
+  getAllProducts(req, res) {
+    const products = ProductModel.getAllProducts();
+    return res.status(200).json(products);
+  }
+
+  //get a product
+  getProductById(req, res) {
+    const productId = req.params.id;
+
+    const product = ProductModel.getProductById(productId);
+
+    if (product) {
+      return res.status(200).json(product);
+    } else {
+      return res.status(404).send("product not found!!!!!!!!!!");
     }
+  }
 
-    //get a product
-    getProductById(req,res){
-        const productId = req.params.id;
+  //add product
+  addProduct(req, res) {
+    console.log(req.body);
 
-        const product = ProductModel.getProductById(productId);
+    const { name, description, category, price } = req.body;
+    const imageUrl = req.file ? `/imageFiles/${req.file.filename}` : null;
 
-        if(product){
-            return res.status(200).json(product);
-        }
-        else{
-            return res.status(404).send('product not found!!!!!!!!!!');
-        }
+    ProductModel.addProduct(name, description, imageUrl, category, price);
+    const products = ProductModel.getAllProducts();
+    return res.status(201).json(products);
+  }
+
+  //filtering products
+  getFilteredProducts(req, res) {
+    console.log("req.query", req.query);
+    const { minPrice, maxPrice, category } = req.query;
+    const filteredProducts = ProductModel.getfilteredProducts(
+      minPrice,
+      maxPrice,
+      category
+    );
+    return res.status(301).json(filteredProducts);
+  }
+
+  // rate produt
+  rateProduct(req, res) {
+    //destructuring
+    const { rating, productId } = req.body;
+    const userId = req.userId;
+    if (!rating || !productId) {
+      return res.status(400).send("bad request");
     }
-
-    //add product
-    addProduct(req,res){
-        console.log(req.body);
-
-        const{name, description, category, price} = req.body;
-        const imageUrl = req.file ? `/imageFiles/${req.file.filename}` : null;
-
-        ProductModel.addProduct(name, description, imageUrl, category, price);
-        const products = ProductModel.getAllProducts();
-        return res.status(201).json(products);
+    //  rating  between 1 and 5
+    if (rating < 1 || rating > 5) {
+      return res.status(400).send("Rating must be 1 to 5 only");
     }
+    //try{
+    ProductModel.rateProduct(rating, userId, productId);
+    return res.status(200).send("rating added successfully");
+    //}catch(e){
+    //    console.log(`error : ${e}`)
+    //    return res.status(400).send(e.message);
+    //}
+  }
 
-    //filtering products
-    getFilteredProducts(req, res){
-        console.log("req.query", req.query);
-        const {minPrice, maxPrice, category} = req.query;
-        const filteredProducts = ProductModel.getfilteredProducts(minPrice, maxPrice, category);
-        return res.status(301).json(filteredProducts);
+  //update product
+  updateProduct(req, res) {
+    console.log(req.body);
+    const productId = req.params.id;
+
+    const imageUrl = req.file
+      ? `/imageFiles/${req.file.filename}`
+      : req.body.imageUrl;
+
+    const updatedData = {
+      name: req.body.name,
+      description: req.body.description,
+      imageUrl: imageUrl,
+      category: req.body.category,
+      price: req.body.price,
+    };
+    const updatedProduct = ProductModel.updateProduct(productId, updatedData);
+    if (updatedProduct) {
+      return res.status(201).json(updatedProduct);
+    } else {
+      return res.status(404).send("Product not found!");
     }
+  }
 
-    // rate produt
-    rateProduct(req, res){
-        //destructuring
-        const {rating, productId} = req.body;
-        const userId = req.userId;
-        if(!rating || !productId){
-            return res.status(400).send("bad request");
-        }
-        //  rating  between 1 and 5
-        if (rating < 1 || rating > 5) {
-            return res.status(400).send("Rating must be 1 to 5 only");
-        }
-        //try{
-            ProductModel.rateProduct(rating, userId, productId);
-            return res.status(200).send("rating added successfully")
-        //}catch(e){
-        //    console.log(`error : ${e}`)
-        //    return res.status(400).send(e.message);
-        //}
+  //delete product
+  deleteProduct(req, res) {
+    //stroing id
+    const productId = req.params.id;
+
+    const isDeleted = ProductModel.deleteProduct(productId); //we get true/false from this method
+
+    if (isDeleted) {
+      const products = ProductModel.getAllProducts();
+      return res.status(201).json(products);
+    } else {
+      return res.status(404).send("no product found");
     }
-
-    //update product
-    updateProduct(req,res){
-        console.log(req.body);
-        const productId = req.params.id;
-
-        const imageUrl = req.file ? `/imageFiles/${req.file.filename}` : req.body.imageUrl;
-
-        const updatedData = {
-            name : req.body.name,
-            description : req.body.description,
-            imageUrl : imageUrl,
-            category : req.body.category,
-            price : req.body.price
-        }
-        const updatedProduct = ProductModel.updateProduct(productId, updatedData);
-        if(updatedProduct){
-            return res.status(201).json(updatedProduct);
-        }
-        else {
-            return res.status(404).send('Product not found!');
-        }
-
-    }
-
-    //delete product
-    deleteProduct(req,res){
-
-        //stroing id 
-        const productId = req.params.id;
-
-        const isDeleted = ProductModel.deleteProduct(productId);//we get true/false from this method
-
-        if(isDeleted){
-            const products = ProductModel.getAllProducts();
-            return res.status(201).json(products);
-        }
-        else{
-            return res.status(404).send("no product found");
-        }
-    }
+  }
 }
